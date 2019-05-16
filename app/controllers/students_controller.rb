1f6599f55493fa28015cf1b2f1e088a0c149763a
Adam Sultanov <adam.sultanov@gmail.com>
2019-05-16 17:42:51 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
   def show
+    if @student.active
+      @activated = "active"
+    else
+      @activated = "inactive"
+    end
+  end
+
+  def activate
+    @student.activate
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,9 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+    status = !self.active
+    self.update(active: status)
+  end
 end
